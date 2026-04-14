4bb8a10417a3385e5c041b4e446b0ef4b4df0ce0
Taha <taha64686@gmail.com>
2026-04-14 06:25:17 +0200

    cart and sidebar

--- app/loading.js
+++ app/loading.js
@@ -4,7 +4,8 @@ import Footer from "./_components/Footer";
 
 export default function Loading() {
   return (
-    <div className="min-h-screen bg-blue-500">
+    <div className="min-h-screen ">
+      <Navbar />
       <div className="h-50 flex items-center justify-center">
         <div className="w-5 h-5 border-t rounded-[50%] rotate-360 animate-spin"></div>
       </div>
